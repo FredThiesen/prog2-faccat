e0dc6c6a68e3518ba542d7f06021006c02e1fdd2
Fred <ricardofredt@hotmail.com>
2022-03-30 18:45:58 -0300

    fixed 5

--- 5.c
+++ 5.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 //Escreva um programa que imprime em tela os números (de 1 à 1000) cuja raiz quadrada são números ímpares.
 
+//checa se a raiz quadrada do número é inteiro (para ser ímpar, precisa ser inteiro)
+int isRootInteger(int numero){
+    if(numero == sqrt(numero) * (int)sqrt(numero)){
+        return 1;
+    }
+    return 0;
+}
+
+//checa se o número é ímpar
+int isOdd(int numero){
+    if(numero % 2 == 1){
+        return 1;
+    }
+    return 0;
+}
+
 int main(void){
-    int numero, quadrado, resto;
+    int numero;
     for(numero = 1; numero <= 1000; numero++){
-        quadrado = numero * numero;
-        resto = quadrado % 2;
-        //se resto for diferente de 0 => ele é ímpar, então imprime o número
-        if(resto != 0){
+        if(isRootInteger(numero) && isOdd(numero)){
             printf("%d\n", numero);
         }
     }
